ca86bbafa3fb6205fb9b15731f22c57ac61ac8a5
csGrebo <oberg.chris@yahoo.com>
2025-11-19 17:26:33 -0600

    fix: change header allocation to check for null pointer

--- src/parse.c
+++ src/parse.c
@@ -15,7 +15,7 @@ int create_db_header(int fd, struct dbheader_t **headerOut) {
         return STATUS_ERROR;
     }
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == -1) {
+    if (header == NULL) {
         printf("Calloc failed to create db header\n");
         return STATUS_ERROR;
     }
@@ -34,7 +34,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
     }
 
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == -1) {
+    if (header == NULL) {
         printf("Calloc failed to create db header\n");
         return STATUS_ERROR;
     }
